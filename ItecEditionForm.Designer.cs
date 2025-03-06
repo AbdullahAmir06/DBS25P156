f13303880f2f67b3614efe39122b0b9441da0ceb
AbdullahAmir06 <aa727dar@gmail.com>
2025-03-07 01:52:08 +0500

    Frontend

---
+++ ItecEditionForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace DBS25P156
+{
+    partial class ItecEditionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ItecEditionForm));
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label6 = new Label();
+            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panel1 = new Panel();
+            label7 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            comboBox1 = new ComboBox();
+            label9 = new Label();
+            label10 = new Label();
+            textBox1 = new TextBox();
+            comboBox2 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.Location = new Point(588, 9);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(27, 25);
+            pictureBox4.TabIndex = 10;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.Location = new Point(398, 9);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(27, 25);
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(271, 9);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(27, 25);
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(153, 8);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(27, 25);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Cursor = Cursors.Hand;
+            label6.FlatStyle = FlatStyle.Flat;
+            label6.Font = new Font("Poppins", 12F);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(298, 6);
+            label6.Margin = new Padding(2);
+            label6.Name = "label6";
+            label6.Size = new Size(78, 36);
+            label6.TabIndex = 5;
+            label6.Text = "Events";
+            label6.MouseLeave += label6_MouseLeave;
+            label6.MouseHover += label6_MouseHover;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Cursor = Cursors.Hand;
+            label5.FlatStyle = FlatStyle.Flat;
+            label5.Font = new Font("Poppins", 12F);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(616, 6);
+            label5.Margin = new Padding(2);
+            label5.Name = "label5";
+            label5.Size = new Size(115, 36);
+            label5.TabIndex = 4;
+            label5.Text = "Payments";
+            label5.MouseLeave += label5_MouseLeave;
+            label5.MouseHover += label5_MouseHover;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Hand;
+            label3.FlatStyle = FlatStyle.Flat;
+            label3.Font = new Font("Poppins", 12F);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(182, 6);
+            label3.Margin = new Padding(2);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 36);
+            label3.TabIndex = 2;
+            label3.Text = "HOME";
+            label3.MouseLeave += label3_MouseHover;
+            label3.MouseHover += label3_MouseHover;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Cursor = Cursors.Hand;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("Poppins", 12F);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(425, 6);
+            label2.Margin = new Padding(2);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 36);
+            label2.TabIndex = 1;
+            label2.Text = "Committees";
+            label2.MouseLeave += label2_MouseLeave;
+            label2.MouseHover += label2_MouseHover;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(6, -1);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 50);
+            label1.TabIndex = 0;
+            label1.Text = "ITEC";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(917, 50);
+            panel1.TabIndex = 5;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Cursor = Cursors.Hand;
+            label7.FlatStyle = FlatStyle.Flat;
+            label7.Font = new Font("Poppins", 12F);
+            label7.ForeColor = SystemColors.ButtonHighlight;
+            label7.Location = new Point(807, 7);
+            label7.Name = "label7";
+            label7.Size = new Size(97, 36);
+            label7.TabIndex = 6;
+            label7.Text = "LOGOUT";
+            label7.MouseLeave += label7_MouseLeave;
+            label7.MouseHover += label7_MouseHover;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(479, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 36);
+            label4.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Font = new Font("Poppins", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = SystemColors.ButtonHighlight;
+            groupBox1.Location = new Point(248, 73);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(430, 345);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Edition Form";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Poppins", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(24, 40);
+            label8.Name = "label8";
+            label8.Size = new Size(40, 23);
+            label8.TabIndex = 7;
+            label8.Text = "Year";
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.LightGray;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(24, 66);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(208, 38);
+            comboBox1.TabIndex = 8;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Poppins", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(24, 107);
+            label9.Name = "label9";
+            label9.Size = new Size(54, 23);
+            label9.TabIndex = 9;
+            label9.Text = "Theme";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Poppins", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(24, 174);
+            label10.Name = "label10";
+            label10.Size = new Size(83, 23);
+            label10.TabIndex = 10;
+            label10.Text = "Description";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.LightGray;
+            textBox1.Location = new Point(24, 200);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(367, 40);
+            textBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            comboBox2.BackColor = Color.LightGray;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(24, 133);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(208, 38);
+            comboBox2.TabIndex = 12;
+            // 
+            // ItecEditionForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(10, 25, 47);
+            ClientSize = new Size(917, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(panel1);
+            Name = "ItecEditionForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ItecEditionForm";
+            Load += ItecEditionForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label6;
+        private Label label5;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Panel panel1;
+        private Label label7;
+        private Label label4;
+        private GroupBox groupBox1;
+        private Label label8;
+        private ComboBox comboBox1;
+        private Label label9;
+        private TextBox textBox1;
+        private Label label10;
+        private ComboBox comboBox2;
+    }
+}
--- obj/Debug/net8.0-windows/DBS25P156.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/DBS25P156.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBS25P156")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1013d07b6a3eeba7a5b254a9b1629142222ca2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81a35a0121e557a0c58c4ab48b5b781a7fc76e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBS25P156")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBS25P156")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
