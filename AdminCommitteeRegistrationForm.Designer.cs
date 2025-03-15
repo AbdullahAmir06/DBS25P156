0aea9049a5e8b05eea76616929011b70d69ad80c
AbdullahAmir06 <aa727dar@gmail.com>
2025-03-16 00:59:47 +0500

    Frontend

--- AdminCommitteeRegistrationForm.Designer.cs
+++ AdminCommitteeRegistrationForm.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminCommitteeRegistrationForm";
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            Select = new DataGridViewCheckBoxColumn();
+            Name = new DataGridViewTextBoxColumn();
+            Category = new DataGridViewTextBoxColumn();
+            Role = new DataGridViewComboBoxColumn();
+            Duty = new DataGridViewTextBoxColumn();
+            Deadline = new DataGridViewTextBoxColumn();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = SystemColors.ButtonHighlight;
+            groupBox1.Location = new Point(38, 54);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(841, 346);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Add New Committee";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poppins", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(17, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Committee Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Poppins", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(17, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(288, 33);
+            textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = Color.LightGray;
+            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Select, Name, Category, Role, Duty, Deadline });
+            dataGridView1.Location = new Point(17, 131);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(803, 181);
+            dataGridView1.TabIndex = 2;
+            // 
+            // Select
+            // 
+            Select.HeaderText = "Select";
+            Select.MinimumWidth = 6;
+            Select.Name = "Select";
+            // 
+            // Name
+            // 
+            Name.HeaderText = "Name";
+            Name.MinimumWidth = 6;
+            Name.Name = "Name";
+            Name.ReadOnly = true;
+            Name.Width = 125;
+            // 
+            // Category
+            // 
+            Category.HeaderText = "Category";
+            Category.MinimumWidth = 6;
+            Category.Name = "Category";
+            Category.ReadOnly = true;
+            Category.Width = 125;
+            // 
+            // Role
+            // 
+            Role.HeaderText = "Role";
+            Role.Items.AddRange(new object[] { "Faculty Advisor", "Student Lead", "Volunteer" });
+            Role.MinimumWidth = 6;
+            Role.Name = "Role";
+            // 
+            // Duty
+            // 
+            Duty.HeaderText = "Duty";
+            Duty.MinimumWidth = 6;
+            Duty.Name = "Duty";
+            Duty.Width = 125;
+            // 
+            // Deadline
+            // 
+            dataGridViewCellStyle2.Format = "f";
+            dataGridViewCellStyle2.NullValue = null;
+            Deadline.DefaultCellStyle = dataGridViewCellStyle2;
+            Deadline.HeaderText = "Deadline";
+            Deadline.MinimumWidth = 6;
+            Deadline.Name = "Deadline";
+            Deadline.Width = 175;
+            // 
+            // AdminCommitteeRegistrationForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(10, 25, 47);
+            ClientSize = new Size(928, 450);
+            Controls.Add(groupBox1);
+            Name = "AdminCommitteeRegistrationForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AdminCommitteeRegistrationForm";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridViewCheckBoxColumn Select;
+        private DataGridViewTextBoxColumn Name;
+        private DataGridViewTextBoxColumn Category;
+        private DataGridViewComboBoxColumn Role;
+        private DataGridViewTextBoxColumn Duty;
+        private DataGridViewTextBoxColumn Deadline;
     }
 }
--- AdminCommitteeRegistrationForm.cs
+++ AdminCommitteeRegistrationForm.cs
@@ -16,5 +16,10 @@ namespace DBS25P156
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
